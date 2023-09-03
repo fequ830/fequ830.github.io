2b589d8a427556c61a366c418a5eed46995ca368
Felix Qu <fequ830@gmail.com>
2023-09-02 21:06:10 -0700

    many fixes

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const Home = () => {
             <div class="line"><p>**</p></div>
             {/* <div class="line"><p>*</p></div> */}
           </div>
-      <div>
+      <div style={{ marginTop: '75vh'}}>
         <h1>
           <span
             className={`tab ${activeTab === 'welcome' ? 'active' : ''}`}
@@ -74,7 +74,7 @@ const Home = () => {
           </span>
         </h1>
       </div>
-      <LanguageSwitcher activeTab={activeTab} />
+      <LanguageSwitcher activeTab={activeTab}/>
     </div>
   );
 };
@@ -84,7 +84,10 @@ const About = () => (
     <h1>About</h1>
     <img src="/felixpic.jpg" alt="Felix Qu Picture" />
     <br></br>
-    <p>Me on top of the Torre Guigini in Lucca, Italy</p>
+    <p>
+      <span className="tower-text">Me on top of the Guinigi Tower in Lucca, Italy</span>
+    </p>
+    <br></br>
     <p>I'm a 3rd year Ling+CS major at UCLA. Always interested in semantic analysis and AI</p>
     <p>Languages: Python, C++, HTML/CSS, JavaScript (React.js), Bash</p>
     <p>Tools: Visual Studio Code, XCode, Git</p>
@@ -126,17 +129,52 @@ const Contact = () => (
   </div>
 );
 
-const Projects = () => (
-  <div className='projects'>
-    <h1>Projects</h1>
-    <p>LeashedIn</p>
-    <p>Boelter Hall Horror Game</p>
-    <p>Peach Party</p>
-    <p>Pomododo</p>
-    <p>Congress Analyzer (coming soon)</p>
 
-  </div>
-);
+const Projects = () => {
+  const projectList = [
+    { name: 'LeashedIn', description:
+    <div>
+      <p>A collaboration on a social media web application for pet owners, inspired by LinkedIn and Instagram</p>
+      <p>Users are able to create an account, add an account bio/pet subprofiles, publicly post text/images on their feed, like and comment on posts</p>
+      <p>Real-time server results include checking username availability and displaying homepage search bar entries</p>
+    </div> },
+    { name: 'Boelter Hall Horror Game', description:
+    <div>
+      <p>A first-person horror game where the player must try to escape a haunted version of UCLA’s Boelter Hall</p>
+      <p>Created 3D assets such as poster boards and animated TV static to be placed around the map</p>
+    </div> },
+    { name: 'Peach Party', description:
+    <div>
+    <p>Created a two-player offline game inspired by Nintendo's Mario Party</p>
+    <p>Sprites are called upon to change game state depending on player action/interaction with other sprites per tick</p>
+    <p>OpenGL API is utilized to render objects in-game each tick</p>
+  </div> },
+    { name: 'Pomododo', description:
+    <div>
+    <p>Developed Pomododo, a web app to help students efficiently split their studying sessions and break times</p>
+    <p>Uses Google authentication for profiles so users can track their total hours studied in Firebase server</p>
+    <p>Main page has a timer that alternates between study and break modes; features some preset timers of popular study-break intervals like the Pomodoro method, and the option to set custom intervals</p>
+  </div> },
+    { name: 'Congress Analyzer (coming soon)', description:
+    <div>
+    <p>A tool using the Congress.gov API to analyze the voting patterns of Congressmembers and amendment votes</p>
+  </div>},
+  ];
+
+  return (
+    <div className='projects'>
+      <h1>Projects</h1>
+      <ul>
+        {projectList.map((project, index) => (
+          <li key={index} className="project-item">
+            <span className="project-name">{project.name}</span>
+            <span className="project-description">{project.description}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 
 
--- src/LanguageSwitcher.js
+++ src/LanguageSwitcher.js
@@ -5,12 +5,12 @@ const LanguageSwitcher = ({ activeTab }) => {
   const content = activeTab === 'welcome' ? (
     <>
       <p>Linguistics+CS @ UCLA</p>
-      <p>The design of this site is an ode to my all-time favorite mystery game: Return of the Obra Dinn. I also wanted to have the main page in two languages, which you can toggle by clicking "Welcome" or "Bienvenue".</p>
+      <p>The design of this site is an ode to my all-time favorite mystery game: <a href="https://store.steampowered.com/app/653530/Return_of_the_Obra_Dinn/" target="_blank" rel="noopener noreferrer" className="underline-on-hover">Return of the Obra Dinn</a>. I also wanted to have the main page in two languages, which you can toggle by clicking "Welcome" or "Bienvenue".</p>
     </>
   ) : (
     <>
       <p>Linguistique+Informatique @ UCLA</p>
-      <p>La conception de cet site web est inspirée par mon jeu vidéo mystère préféré : Return of the Obra Dinn. J'ai voulu aussi d'avoir l'option pour mettre le page principal en deux langues; vous pouvez l'ajuster en cliquant sur « Welcome » ou « Bienvenue » au dessus.</p>
+      <p>La conception de cet site web est inspirée par mon jeu vidéo mystère préféré : <a href="https://store.steampowered.com/app/653530/Return_of_the_Obra_Dinn/" target="_blank" rel="noopener noreferrer" className="underline-on-hover">Return of the Obra Dinn</a>. J'ai voulu aussi d'avoir l'option pour mettre le page principal en deux langues; vous pouvez l'ajuster en cliquant sur « Welcome » ou « Bienvenue » au-dessus.</p>
     </>
   );
 
